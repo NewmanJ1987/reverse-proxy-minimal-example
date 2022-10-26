eb1be0977ec6619cdec13eb7fcc44f214201fa50
Newman Thurairatnam <newman.thurairatnam@gameloft.com>
2022-10-25 20:22:44 -0400

    trying stuff out

--- server/wsgi.py
+++ server/wsgi.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 app.register_blueprint(example_blueprint)
 
 def run_proxy():
+    # return app
     return ProxyFix(app, x_for=1, x_host=1, x_port=1, x_proto=1, x_prefix=1)
     
